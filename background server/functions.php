29423bca3239f53d0bb6c4a08c7204f4fdd647db
xuejingwei <xuejingwei_1129@outlook.com>
2019-11-28 17:42:15 +0800

    some updates and fixes to speed up the site

--- background server/functions.php
+++ background server/functions.php
@@ -1,21 +1,6 @@
 <?php
 
-/*添加进主题文件functions.php的内容，用来移除wordpress自带的不必要功能*/
-
-remove_action( 'wp_head', 'rsd_link' );
-remove_action( 'wp_head', 'wlwmanifest_link' );
-remove_action( 'wp_head', 'index_rel_link' );
-remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
-remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
-remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
-remove_action( 'wp_head', 'feed_links', 2 );
-remove_action( 'wp_head', 'feed_links_extra', 3 ); 
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
-
-
-/*jQuery加速*/
+/*jQuery加速，移除WordPress内联jQuery，这个没法用defer属性*/
 wp_deregister_script('jquery'); 
-wp_register_script('jquery', 'https://cdn.jsdelivr.net/gh/jquery/jquery@1.12.4/dist/jquery.min.js', false, '1.12.4'); 
+wp_register_script('jquery', '', true, '1.12.4'); 
 wp_enqueue_script('jquery'); 
---
+++ background server/live2d.php
@@ -0,0 +1,8 @@
+<?php
+/*修改过后的live2d插件index.php文件，jQuery以及live2d库加载使用defer形式，不阻塞*/
+
+/*112行*/
+
+<script type="text/javascript" defer="" src="https://cdn.jsdelivr.net/gh/jquery/jquery@1.12.4/dist/jquery.min.js"></script>
+<script type="text/javascript" defer="" src="https://cdn.jsdelivr.net/gh/xb2016/kratos-pjax@0.3.6/static/js/live2d.js"></script>
+
