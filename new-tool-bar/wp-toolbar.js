da9bef9290b056836abb1a7fef231d1129f987a5
xuejingwei <xuejingwei_1129@outlook.com>
2020-03-22 23:43:44 +0800

    工具栏整合更换壁纸、背景音乐、昼夜主题功能

--- new-tool-bar/wp-toolbar.js
+++ new-tool-bar/wp-toolbar.js
@@ -1,10 +1,5 @@
-var themeOption = {
-	'font-color': 'coral',
-	'bg-color': 'beige',
-	'border-shadow-color': 'lightcoral',
-	'bg-hover-color': 'lightpink'
-};
 window.CustomizeToolBar = window.CustomizeToolBar || {};
+CustomizeToolBar.themeOption = CustomizeToolBar.themeOption || {};
 CustomizeToolBar.classTokenList = {
 	visibility: {
 		toolBarHide: 'customize-toolbar-hide-all',
@@ -44,6 +39,266 @@ CustomizeToolBar.domList = {
 };
 CustomizeToolBar.width = 200;
 CustomizeToolBar.height = 200;
+(function() {
+	CustomizeToolBar.toggleCssClass = function(dom, className) {
+		var classList = dom.classList;
+		classList.contains(className)
+			? classList.remove(className)
+			: classList.add(className);
+	};
+	//Add mousedown and mouseup effect to all buttons
+	var buttons = CustomizeToolBar.domList.buttonDomList;
+	Object.keys(buttons).forEach(function(button) {
+		var _this = buttons[button];
+		_this.addEventListener('mousedown', function() {
+			CustomizeToolBar.toggleCssClass(
+				_this,
+				CustomizeToolBar.classTokenList.visibility.iconClick
+			);
+		});
+		_this.addEventListener('mouseup', function() {
+			CustomizeToolBar.toggleCssClass(
+				_this,
+				CustomizeToolBar.classTokenList.visibility.iconClick
+			);
+		});
+	});
+	//back to top
+	CustomizeToolBar.domList.buttonDomList.backToTopButton.addEventListener(
+		'click',
+		function() {
+			window.scrollTo({ top: 0, behavior: 'smooth' });
+			//TODO: Make the scroll speed under my control
+		}
+	);
+	//close the menu
+	CustomizeToolBar.domList.buttonDomList.closeMenuButton.addEventListener(
+		'click',
+		function() {
+			CustomizeToolBar.toggleCssClass(
+				CustomizeToolBar.domList.menuAreaDom,
+				CustomizeToolBar.classTokenList.visibility.menuHide
+			);
+		}
+	);
+	CustomizeToolBar.domList.blankAreaDom.addEventListener('click', function(
+		e
+	) {
+		e.stopPropagation();
+		if (e.detail === 1) {
+			CustomizeToolBar.toggleCssClass(
+				CustomizeToolBar.domList.menuAreaDom,
+				CustomizeToolBar.classTokenList.visibility.menuHide
+			);
+		}
+		return;
+	});
+	//switch background wallpaper to bing image
+	function switchPicAnimation() {
+		CustomizeToolBar.toggleCssClass(
+			CustomizeToolBar.domList.corePicDom,
+			CustomizeToolBar.classTokenList.rotation.picMove
+		);
+	}
+	function getLocalBingWallpaper() {
+		var localCache = localStorage.getItem('today-bing-image');
+		if (localCache) {
+			var localObj = JSON.parse(local);
+			if (new Date(localObj.time).getDate() !== new Date().getDate()) {
+				getBingWallpaper();
+			} else {
+				//add image loading effect
+				switchPicAnimation();
+				var temp_img = document.createElement('img');
+				temp_img.setAttribute('src', localObj.url);
+				temp_img.addEventListener('load', function() {
+					var bodyRef = document.body;
+					if (!bodyRef.style.transition) {
+						bodyRef.style.transition =
+							'background-position-x 2s linear';
+					}
+					bodyRef.classList.add('bingwallpaper-bg-start');
+					setTimeout(function() {
+						bodyRef.classList.add('bingwallpaper-bg-end');
+						bodyRef.style.backgroundImage = `url(${localObj.url})`;
+						switchPicAnimation();
+					}, 2000);
+				});
+			}
+		} else {
+			getBingWallpaper();
+		}
+	}
+	async function getBingWallpaper() {
+		return new Promise(function(resolve, reject) {
+			switchPicAnimation();
+			var xhr = new XMLHttpRequest();
+			xhr.open(
+				'GET',
+				'https://api.littlesecret.cn/wordpress/bingwallpaper'
+			);
+			xhr.responseType = 'text';
+			xhr.onload = function() {
+				if (xhr.readyState === 4 && xhr.status === 200) {
+					resolve(xhr.responseText);
+				} else {
+					reject('xhr request has no valid response');
+				}
+			};
+			xhr.onerror = function() {
+				reject('there was a network error');
+			};
+			xhr.send();
+		})
+			.then(function(result) {
+				var temp_img = document.createElement('img');
+				temp_img.setAttribute('src', result);
+				temp_img.addEventListener('load', function() {
+					var bodyRef = document.body;
+					if (!bodyRef.style.transition) {
+						bodyRef.style.transition =
+							'background-position-x 2s linear';
+					}
+					bodyRef.classList.add('bingwallpaper-bg-start');
+					setTimeout(function() {
+						bodyRef.classList.add('bingwallpaper-bg-end');
+						bodyRef.style.backgroundImage = `url(${result})`;
+						switchPicAnimation();
+					}, 2000);
+					var store = {
+						url: result,
+						time: new Date().getTime()
+					};
+					localStorage.setItem(
+						'today-bing-image',
+						JSON.stringify(store)
+					);
+				});
+			})
+			.catch(function(err) {
+				console.log(`Get image failed for the error '${err}'`);
+			});
+	}
+	CustomizeToolBar.domList.buttonDomList.bingImageButton.addEventListener(
+		'click',
+		function(e) {
+			getLocalBingWallpaper();
+			e.preventDefault();
+		}
+	);
+	//background music
+	var audioBtn = CustomizeToolBar.domList.buttonDomList.bgmControlButton;
+	var audioIcon = audioBtn.getElementsByTagName('i')[0];
+	var audioTip = audioBtn.nextElementSibling;
+	audioBtn.addEventListener('click', function() {
+		var audioDom = document.querySelector('audio');
+		if (audioDom.paused || audioDom.played.length === 0) {
+			audioDom.play();
+			audioIcon.setAttribute(
+				'class',
+				CustomizeToolBar.classTokenList.toolBarIcon.bgmPause
+			);
+			audioTip.innerHTML = '暂停背景音乐';
+		} else {
+			audioDom.pause();
+			audioIcon.setAttribute(
+				'class',
+				CustomizeToolBar.classTokenList.toolBarIcon.bgmPlay
+			);
+			audioTip.innerHTML = '播放背景音乐';
+		}
+	});
+})();
+(function() {
+	if (!location.href.startsWith('https://www.littlemeteor.me/wp-')) {
+		var hour = new Date().getHours();
+		var dom = document.getElementById('time-period-greeting');
+		if (dom) {
+			var text;
+			switch (true) {
+				case hour >= 0 && hour < 6:
+					text = '凌晨好';
+					break;
+				case hour >= 6 && hour <= 7:
+					text = '早安';
+					break;
+				case hour > 7 && hour < 11:
+					text = '上午好';
+					break;
+				case hour >= 11 && hour < 13:
+					text = '中午好';
+					break;
+				case hour >= 13 && hour < 18:
+					text = '下午好';
+					break;
+				case hour >= 18 && hour <= 21:
+					text = '晚上好';
+					break;
+				case hour >= 22 && hour <= 23:
+					text = '晚安';
+					break;
+			}
+			document.getElementById('time-period-greeting').innerHTML =
+				text + '，亲爱的访客';
+		}
+		var theme_image = document.getElementById('day-night-theme-image');
+		var bodyRef = document.body;
+		//day theme
+		if (hour >= 6 && hour < 18) {
+			bodyRef.classList.add('body-bg-day');
+			bodyRef.style.setProperty('--widget-bg-color', 'rgb(247,247,220)');
+			if (theme_image) {
+				theme_image.setAttribute(
+					'src',
+					'https://static.littlesecret.cn/images/inserts/2c9883c0c3d7b75e0a3feee8354ddd9b_optimized.png'
+				);
+			}
+			CustomizeToolBar.domList.blankAreaDom
+				.getElementsByTagName('i')[0]
+				.setAttribute(
+					'class',
+					CustomizeToolBar.classTokenList.toolBarIcon.dayBg
+				);
+			CustomizeToolBar.themeOption = {
+				'font-color': 'coral',
+				'bg-color': 'beige',
+				'border-shadow-color': 'lightcoral',
+				'bg-hover-color': 'lightpink'
+			};
+		}
+		//night theme
+		else {
+			bodyRef.classList.add('body-bg-night');
+			bodyRef.style.setProperty('--widget-bg-color', 'rgb(81,99,123)');
+			bodyRef.style.setProperty('--widget-font-color', '#ffb5a1');
+			bodyRef.style.setProperty('--widget-title-color', 'lightcoral');
+			bodyRef.style.setProperty('--widget-list-item-color', 'bisque');
+			if (theme_image) {
+				theme_image.setAttribute(
+					'src',
+					'https://static.littlesecret.cn/images/inserts/56f8dc7323d843977ec483846d01be74.png'
+				);
+			}
+			CustomizeToolBar.domList.blankAreaDom
+				.getElementsByTagName('i')[0]
+				.setAttribute(
+					'class',
+					CustomizeToolBar.classTokenList.toolBarIcon.nightBg
+				);
+			CustomizeToolBar.themeOption = {
+				'font-color': 'coral',
+				'bg-color': 'beige',
+				'border-shadow-color': 'lightcoral',
+				'bg-hover-color': 'lightpink'
+			};
+		}
+		//initialize bgm
+		var au = document.createElement('audio');
+		au.src = 'https://static.littlesecret.cn/sounds/20191121103956.mp3';
+		au.loop = true;
+		document.body.appendChild(au);
+	}
+})();
 (function(opt) {
 	CustomizeToolBar.setCustomizeColor = function() {
 		var styleList = this.domList.toolBarDom.style;
@@ -66,70 +321,16 @@ CustomizeToolBar.height = 200;
 			  )
 			: {};
 	};
-})(themeOption);
-(function() {
-	CustomizeToolBar.toggleCssClass = function(dom, className) {
-		var classList = dom.classList;
-		classList.contains(className)
-			? classList.remove(className)
-			: classList.add(className);
-	};
-	CustomizeToolBar.toggleCorePicRotateY = function() {
-		this.toggleCssClass(
-			this.domList.corePicDom,
-			this.classTokenList.rotation.bgMove
-		);
-	};
-	var buttons = CustomizeToolBar.domList.buttonDomList;
-	Object.keys(buttons).forEach(function(button) {
-		var _this = buttons[button];
-		_this.addEventListener('mousedown', function() {
-			CustomizeToolBar.toggleCssClass(
-				_this,
-				CustomizeToolBar.classTokenList.visibility.iconClick
-			);
-		});
-		_this.addEventListener('mouseup', function() {
-			CustomizeToolBar.toggleCssClass(
-				_this,
-				CustomizeToolBar.classTokenList.visibility.iconClick
-			);
-		});
-	});
-})();
-(function() {
-	CustomizeToolBar.domList.buttonDomList.backToTopButton.addEventListener(
-		'click',
-		function() {
-			window.scrollTo({ top: 0, behavior: 'smooth' });
-		}
+	CustomizeToolBar.setCustomizeColor();
+})(CustomizeToolBar.themeOption);
+window.onload = function() {
+	CustomizeToolBar.toggleCssClass(
+		CustomizeToolBar.domList.menuAreaDom,
+		CustomizeToolBar.classTokenList.visibility.menuHide
 	);
-	CustomizeToolBar.domList.buttonDomList.closeMenuButton.addEventListener(
-		'click',
-		function() {
-			CustomizeToolBar.toggleCssClass(
-				CustomizeToolBar.domList.menuAreaDom,
-				CustomizeToolBar.classTokenList.visibility.menuHide
-			);
-		}
-	);
-	CustomizeToolBar.domList.blankAreaDom.addEventListener('click', function(
-		e
-	) {
-		e.stopPropagation();
-		if (e.detail === 1) {
-			CustomizeToolBar.toggleCssClass(
-				CustomizeToolBar.domList.menuAreaDom,
-				CustomizeToolBar.classTokenList.visibility.menuHide
-			);
-		}
-		return;
-	});
-})();
-(function() {
 	var tip = document.querySelector('.customize-toolbar-blank-tip>div');
 	tip.classList.remove('customize-toolbar-hide-tip');
 	setTimeout(function() {
 		tip.classList.add('customize-toolbar-hide-tip');
 	}, 5000);
-})();
+};
