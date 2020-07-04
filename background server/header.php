b38d2ba5c5c51a9d13ab28eeff3cdc425757f5ad
xuejingwei <xuejingwei_1129@outlook.com>
2020-07-05 00:15:32 +0800

    合并自定义CSS，移除陈旧样式，添加DNS预解析

--- background server/header.php
+++ background server/header.php
@@ -1,6 +1,9 @@
-<?php
-/*修改过后的header.php文件，在header部分添加了判断，当页面为首页时不显示自带顶部图像*/
-?>
+<!--添加外部域名预解析-->
+<link rel="dns-prefetch" href="https://static.littlesecret.cn">
+<link rel="dns-prefetch" href="https://wp-cdn.littlesecret.cn">
+<link rel="dns-prefetch" href="https://cdn.jsdelivr.net">
+
+<!--在header部分添加了判断，当页面为首页时不显示自带顶部图像-->
 <div id="header-image-main">
 	<div id="header-image-main-inside">
 		<?php if(is_home()){echo "<div id='header-page-title'><div id='header-page-title-inside'><h2 class='entry-title' itemprop='headline'>小流星的梦</h2><p class='byline'>遇见美好，藏于心间，奔向梦的彼岸</p></div></div>";}		
