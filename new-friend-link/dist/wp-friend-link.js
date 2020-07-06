9690114e3b3fa50271fc2eb236862e417681af4c
xuejingwei <xuejingwei_1129@outlook.com>
2020-07-07 04:29:46 +0800

    自定义友情链接页面添加加载动画，修复了多个window.onload事件覆盖的问题

--- new-friend-link/dist/wp-friend-link.js
+++ new-friend-link/dist/wp-friend-link.js
@@ -1,22 +1,48 @@
-/**
- * 固定url前缀
- */
 const prefix = "https://cdn.jsdelivr.net";
-var linkContainer = document.getElementById("link-container");
+const loadingTip = [
+	"正在加载伙伴信息",
+	"正在加载装饰元素",
+	"正在完成",
+	"出现网络错误，已记录到控制台",
+];
+var loadingStatus = document.querySelector(".loading-status"),
+	loadingText = document.querySelector(".loading-text"),
+	linkContainer = document.getElementById("link-container"),
+	logoCompleted = 0,
+	resFailed = 0;
 function logoLoad(imgDom) {
 	var p = imgDom.parentElement;
 	p.style.backgroundImage = `url(${imgDom.src})`;
 	p.nextElementSibling.style.display = "none";
+	logoCompleted += 1;
+	if (logoCompleted == friendInfo.friends.length) {
+		loadingStatus.innerHTML = loadingTip[1];
+	}
 }
 function logoLoadError(imgDom) {
-	imgDom.parentElement.nextElementSibling.innerHTML =
-		"Logo图片加载失败，连接时间过长或目标站点不存在该图片";
+	imgDom.parentElement.nextElementSibling.innerHTML = "Logo图片加载失败";
+	resFailed += 1;
+	console.log("logo加载失败");
 }
 friendInfo.friends.forEach(function (item) {
 	var linkBlock = document.createElement("div");
 	linkBlock.setAttribute("class", "fl-link");
-	linkBlock.innerHTML = `<div class="fl-link-img"><img src="${prefix}/gh/littlestar520521/wordpress-backup@master/assets/0f7d343835c3074b0f2b22dabb731304.png"/></div><div class="fl-link-content"><div class="fl-link-content-l"><a href="${item.site}" target="_blank" rel="noreferrer noopener"><img src="${item.logo}" onload="logoLoad(this)" onerror="logoLoadError(this)"/></a><div>正在加载Logo图片</div></div><div class="fl-link-content-r"><a href="${item.site}" target="_blank" rel="noreferrer noopener">${item.name}</a><p>${item.description}</p></div></div>`;
+	linkBlock.innerHTML = `<div class="fl-link-img"><img src="${prefix}/gh/littlestar520521/wordpress-backup@1.2/assets/0f7d343835c3074b0f2b22dabb731304.png"/></div><div class="fl-link-content"><div class="fl-link-content-l"><a href="${item.site}" target="_blank" rel="noreferrer noopener"><img src="${item.logo}" onload="logoLoad(this)" onerror="logoLoadError(this)"/></a><div>正在加载Logo图片</div></div><div class="fl-link-content-r"><a href="${item.site}" target="_blank" rel="noreferrer noopener">${item.name}</a><p>${item.description}</p></div></div>`;
 	linkContainer.appendChild(linkBlock);
+});
+window.addEventListener("load", function () {
+	if (resFailed == 0) {
+		loadingStatus.innerHTML = loadingTip[2];
+		loadingText.innerHTML = "大功告成";
+	} else {
+		loadingStatus.innerHTML = loadingTip[3];
+		loadingText.innerHTML = "有些小错";
+	}
+	setTimeout(function () {
+		var ms = document.querySelector(".loading-masks");
+		ms.style.removeProperty("animation");
+		ms.classList.add("hide");
+	}, 2000);
 });
 (function (t, h, u) {
 	if (
@@ -29,7 +55,7 @@ friendInfo.friends.forEach(function (item) {
 		var tip = document.createElement("div");
 		tip.setAttribute(
 			"style",
-			"text-align:center;position:fixed;z-index:100;top:0;left:0;opacity:1;transition:opacity 0.5s linear;width:100%"
+			"text-align:center;position:fixed;z-index:2000;top:0;left:0;opacity:1;transition:opacity 0.5s linear;width:100%"
 		);
 		tip.innerHTML =
 			'<div style="display:inline-block;box-shadow:0 0 3px gray;padding:3px 5px;border-radius:5px;font-size:12px;font-family:sans-serif;line-height:20px;background:#fff;width:80%;margin-top:5px;"><span style="margin-right:5px;color:orange;"><i class="fa fa-exclamation-circle"></i></span><span>检测到移动设备，为节省资源，未启用个性化外观</span></div>';
@@ -63,7 +89,7 @@ friendInfo.friends.forEach(function (item) {
 					var s2 = document.createElement("script");
 					s2.src =
 						prefix +
-						"/gh/littlestar520521/wordpress-backup@1.1.1/optimization/dist/extra.min.js";
+						"/gh/littlestar520521/wordpress-backup@1.2/optimization/dist/extra.min.js";
 					s2.async = true;
 					document.body.appendChild(s2);
 				};
--- new-friend-link/wp-friend-link.js
+++ new-friend-link/wp-friend-link.js
@@ -1,9 +1,9 @@
-var friendInfo = {
+const friendInfo = {
 	friends: [
 		{
 			name: "可乐",
 			site: "https://bozaiq.cn/",
-			logo: "https://cdn.jsdelivr.net/gh/littlestar520521/wordpress-backup/assets/rabbit-logo.png",
+			logo: "../assets/rabbit-logo.png",
 			description: "爱折腾的兔子大佬，总有层出不穷的新点子，每天元气满满",
 		},
 		{
@@ -17,27 +17,36 @@ var friendInfo = {
 		{
 			name: "DannyLiu的博客",
 			site: "http://www.lhlh326.xyz/",
-			logo: "https://cdn.jsdelivr.net/gh/littlestar520521/wordpress-backup/assets/lhlh326-logo.jpg",
+			logo: "../assets/lhlh326-logo.jpg",
 			description:
 				"跟兔子大佬一样的喜欢折腾，他是学C语言的，C语言把我整的够呛，我太菜了",
 		},
 		{
 			name: "谈笑学习笔记",
 			site: "https://www.tanxiao.xyz/",
-			logo: "https://cdn.jsdelivr.net/gh/littlestar520521/wordpress-backup/assets/tanxiao-logo.jpg",
+			logo: "../assets/tanxiao-logo.jpg",
 			description: "他这里全是Java(￣_,￣ )",
 		},
 		{
 			name: "栗子先生的博客",
 			site: "https://blog.lizixs.top/",
-			logo: "https://cdn.jsdelivr.net/gh/littlestar520521/wordpress-backup/assets/lizixs-logo.jpg",
+			logo: "../assets/lizixs-logo.jpg",
 			description:
 				"咳咳，有点可爱的男孩子，和一个未知的小姐姐合作打理一个WordPress站点",
 		},
 	],
 };
-var linkContainer = document.getElementById("link-container");
-
+const loadingTip = [
+	"正在加载伙伴信息",
+	"正在加载装饰元素",
+	"正在完成",
+	"出现网络错误，已记录到控制台",
+];
+var loadingStatus = document.querySelector(".loading-status"),
+	loadingText = document.querySelector(".loading-text"),
+	linkContainer = document.getElementById("link-container"),
+	logoCompleted = 0,
+	resFailed = 0;
 /**
  * logo图片加载
  * @param {HTMLImageElement} imgDom
@@ -46,19 +55,38 @@ function logoLoad(imgDom) {
 	var p = imgDom.parentElement;
 	p.style.backgroundImage = `url(${imgDom.src})`;
 	p.nextElementSibling.style.display = "none";
+	logoCompleted += 1;
+	if (logoCompleted == friendInfo.friends.length) {
+		loadingStatus.innerHTML = loadingTip[1];
+	}
 }
 /**
  * logo图片加载失败
  * @param {HTMLImageElement} imgDom
  */
 function logoLoadError(imgDom) {
-	imgDom.parentElement.nextElementSibling.innerHTML =
-		"Logo图片加载失败，连接时间过长或目标站点不存在该图片";
+	imgDom.parentElement.nextElementSibling.innerHTML = "Logo图片加载失败";
+	resFailed += 1;
+	console.log("logo加载失败");
 }
 
 friendInfo.friends.forEach(function (item) {
 	var linkBlock = document.createElement("div");
 	linkBlock.setAttribute("class", "fl-link");
-	linkBlock.innerHTML = `<div class="fl-link-img"><img src="https://cdn.jsdelivr.net/gh/littlestar520521/wordpress-backup/assets/0f7d343835c3074b0f2b22dabb731304.png"/></div><div class="fl-link-content"><div class="fl-link-content-l"><a href="${item.site}" target="_blank" rel="noreferrer noopener"><img src="${item.logo}" onload="logoLoad(this)" onerror="logoLoadError(this)"/></a><div>正在加载Logo图片</div></div><div class="fl-link-content-r"><a href="${item.site}" target="_blank" rel="noreferrer noopener">${item.name}</a><p>${item.description}</p></div></div>`;
+	linkBlock.innerHTML = `<div class="fl-link-img"><img src="../assets/0f7d343835c3074b0f2b22dabb731304.png"/></div><div class="fl-link-content"><div class="fl-link-content-l"><a href="${item.site}" target="_blank" rel="noreferrer noopener"><img src="${item.logo}" onload="logoLoad(this)" onerror="logoLoadError(this)"/></a><div>正在加载Logo图片</div></div><div class="fl-link-content-r"><a href="${item.site}" target="_blank" rel="noreferrer noopener">${item.name}</a><p>${item.description}</p></div></div>`;
 	linkContainer.appendChild(linkBlock);
+});
+window.addEventListener("load", function () {
+	if (resFailed == 0) {
+		loadingStatus.innerHTML = loadingTip[2];
+		loadingText.innerHTML = "大功告成";
+	} else {
+		loadingStatus.innerHTML = loadingTip[3];
+		loadingText.innerHTML = "有些小错";
+	}
+	setTimeout(function () {
+		var ms = document.querySelector(".loading-masks");
+		ms.style.removeProperty("animation");
+		ms.classList.add("hide");
+	}, 2000);
 });
