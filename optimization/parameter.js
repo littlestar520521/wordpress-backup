434bdeb3cb5628e4dc6246a0015cdc45a1630e40
xuejingwei <xuejingwei_1129@outlook.com>
2020-08-21 01:01:53 +0800

    工具栏挂件背景图设置统一至自定义参数列表中，背景音乐bug修正

---
+++ optimization/parameter.js
@@ -0,0 +1,33 @@
+/* 个性化外观自定义参数配置 */
+const opt = {
+	toolbarTheme: {
+		"font-color": "coral",
+		"bg-color": "beige",
+		"border-shadow-color": "lightcoral",
+		"bg-hover-color": "lightpink",
+		"bg-img-css":
+			"url(https://wp-cdn.bozaiq.cn/uni/images/backgrounds/love/bad27a027fff5ca54756c7da2e6cc756_optimized.png)",
+	},
+	siteTheme: {
+		dayTheme: {
+			image:
+				"https://wp-cdn.bozaiq.cn/uni/images/inserts/2c9883c0c3d7b75e0a3feee8354ddd9b_optimized.png",
+			css: { "--widget-bg-color": "rgb(250,250,235)" },
+		},
+		nightTheme: {
+			image:
+				"https://wp-cdn.bozaiq.cn/uni/images/inserts/56f8dc7323d843977ec483846d01be74.png",
+			css: {
+				"--widget-bg-color": "rgb(81,99,123)",
+				"--widget-font-color": "#ffb5a1",
+				"--widget-title-color": "lightcoral",
+				"--widget-list-item-color": "bisque",
+			},
+		},
+	},
+	bgm: {
+		url: "https://wp-cdn.bozaiq.cn/uni/sounds/snowdream_202051324111.mp3",
+		length: 293,
+	},
+	bingImageApi: "https://www.littlemeteor.me/api/bingwallpaper",
+};
